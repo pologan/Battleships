9a7255fdf7aa7f86a0ad06923fde6120f39208c6
p.gandorski@ideo.pl <p.gandorski@ideo.pl>
2022-05-10 22:03:38 +0200

    Add project files.

---
+++ Battleships/Battleships/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Battleships.ViewModels;
+using Battleships.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Battleships
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Battleships/Battleships/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Battleships.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Battleships.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
